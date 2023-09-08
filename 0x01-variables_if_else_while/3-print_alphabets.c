4d10b7acb038a8c1851e4b09df161e1ebfbdb555
DannyPs4623 <sheyodaniel@gmail.com>
2023-09-08 06:15:42 -0700

    lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Function to print alphabet in lower case and uppercase using putchar
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char low[26] = "abcdefghijklmnopqrstuvwxyz";
+char upp[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i, j;
+
+for (i = 0; i < 26; i++)
+{
+	putchar(low[i]);
+}
+for (j = 0; j < 26; j++)
+{
+	putchar(upp[j]);
+}
+putchar('\n');
+return (0);
+}
+
